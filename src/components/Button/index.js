6d4404fa913b200c9275d47bcebebac239cea1ac
Priscilla Carrenho <priscillaonil@gmail.com>
2022-10-22 18:02:55 -0300

    feat: Init project

---
+++ src/components/Button/index.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+export function Button({ children }) {
+  return <button className="button"> {children} </button>;
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+export function Header() {
+  return (
+    <header>
+      <div>Logo</div>
+
+      <nav>
+        <ul>
+          <li>
+            <span>#</span>home
+          </li>
+          <li>
+            <span>#</span>works
+          </li>
+          <li>
+            <span>#</span>about-me
+          </li>
+          <li>
+            <span>#</span>contacts
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
